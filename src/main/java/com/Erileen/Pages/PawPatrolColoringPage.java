1f65371d9285a55bc36d29d79273852744c91827
Inderjit57 <singhinderjit57@yahoo.com>
2025-01-26 06:24:15 -0500

    Need to debug this page to find element

--- src/main/java/com/Erileen/Pages/PawPatrolColoringPage.java
+++ src/main/java/com/Erileen/Pages/PawPatrolColoringPage.java
@@ -2,6 +2,7 @@ package com.Erileen.Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,12 +14,28 @@ public class PawPatrolColoringPage extends TestBase {
 	public PawPatrolColoringPage() {
 		PageFactory.initElements(wd, this);
 	}
+	
+	Actions action = new Actions(wd);
 
 	@FindBy(css = "#album> div[class='thumbnail side-corner-tag']>a[href='/coloring-page/1679287?album=paw-patrol-coloring-pages']")
 	WebElement skye;
 	
+	@FindBy(css = "#creative>iframe")
+	WebElement iframe;
+	
+	@FindBy(css= "#dismiss-button")
+	WebElement dismissButton;
+	
+	public void switchToAlert() {
+
+//	action.moveToLocation(0, 0)
+
+	}
+	
 	public void clickSkye() {
-		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector("")));
+		wd.navigate().refresh();
+		wait.until(ExpectedConditions.elementToBeClickable(skye));
+//		action.scrollToElement(skye);
 		skye.click();
 	}
 
